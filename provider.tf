9b52ddbc10a50f027f20515500891193572cfe95
RaviVersionControl <89581636+RaviVersionControl@users.noreply.github.com>
2024-05-26 16:29:48 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,9 +6,14 @@ terraform {
    }
  }
 
- backend "s3" {
+backend "s3" {
    bucket = "tf-tuts-state-rrs"
    key    = "state"
    region = "us-east-1"
  }
+}
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAXV4K4TIWXPVCZHAK"
+  secret_key = "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"
 }
